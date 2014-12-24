1fbd262b406bf8fb241aad03ae69e1bc7de34377
dmendoza05 <mendozad05@yahoo.com>
2014-12-23 18:01:38 -0800

    added firebase

--- script/ExoController.js
+++ script/ExoController.js
@@ -5,7 +5,7 @@ angular
 ExoController.$inject = ['$firebase'];
 
 	function ExoController($firebase){
-		controllah = this;
+		var controllah = this;
 		playerTurn = 'X';
 		result = null;
 
@@ -14,34 +14,34 @@ ExoController.$inject = ['$firebase'];
 		this.checkWinX = checkWinX;
 		this.checkWinO = checkWinO;
 		this.resetGame = resetGame;
-		this.gameArray = newGameBoard();
-
+		// this.gameArray = fireGame();
+		newGameBoard();
 		// this.whoWon = whoWon;
 
 		function fireGame() {
      		var ref = new Firebase('https://exo.firebaseio.com/');
       		var flame = $firebase(ref).$asObject();
-
+      		console.log(flame);
       		return flame;
       	}
 
       	//firebase example code
-      	controllah.fireGame.playerOneTurn = true;
-      	controllah.fireGame.playerTwoPresent = true;
-      	controllah.fireGame.gameboard = [[][][]];
-      	controllah.fireGame.$save();
+      	// controllah.fireGame.playerOneTurn = true;
+      	// controllah.fireGame.playerTwoPresent = true;
+      	// controllah.fireGame.gameboard = [[][][]];
+      	// controllah.fireGame.$save();
 
 
 		z = 0;
 		function click(row, col){
-			if(controllah.gameArray[row][col].eXo === ''){
+			if(controllah.fireGame.board[row][col].eXo === ''){
 				if (playerTurn === 'X') {
-					controllah.gameArray[row][col].eXo = 'X';
+					controllah.fireGame.board[row][col].eXo = 'X';
 					playerTurn = 'O'; 
 					checkTie();
 				}
 				else{
-					controllah.gameArray[row][col].eXo = 'O';
+					controllah.fireGame.board[row][col].eXo = 'O';
 					playerTurn = 'X';
 					checkTie();
 				}
@@ -55,33 +55,39 @@ ExoController.$inject = ['$firebase'];
 			}
 			checkWinX();
 			checkWinO();
+
+			controllah.fireGame.$save();
 		};
 
 		function checkWinX(){
 			// Hortizontal Check for X
-			if(controllah.gameArray[0][0].eXo === 'X' && controllah.gameArray[0][1].eXo === 'X' && controllah.gameArray[0][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
-			else if(controllah.gameArray[1][0].eXo === 'X' && controllah.gameArray[1][1].eXo === 'X' && controllah.gameArray[1][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
-			else if(controllah.gameArray[2][0].eXo === 'X' && controllah.gameArray[2][1].eXo === 'X' && controllah.gameArray[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			if(controllah.fireGame.board[0][0].eXo === 'X' && controllah.fireGame.board[0][1].eXo === 'X' && controllah.fireGame.board[0][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[1][0].eXo === 'X' && controllah.fireGame.board[1][1].eXo === 'X' && controllah.fireGame.board[1][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[2][0].eXo === 'X' && controllah.fireGame.board[2][1].eXo === 'X' && controllah.fireGame.board[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
 			// Vertical Check for X
-			else if(controllah.gameArray[0][0].eXo === 'X' && controllah.gameArray[1][0].eXo === 'X' && controllah.gameArray[2][0].eXo === 'X'){return z = 8, controllah.result = "X Won";}
-			else if(controllah.gameArray[0][1].eXo === 'X' && controllah.gameArray[1][1].eXo === 'X' && controllah.gameArray[2][1].eXo === 'X'){return z = 8, controllah.result = "X Won";}
-			else if(controllah.gameArray[0][2].eXo === 'X' && controllah.gameArray[1][2].eXo === 'X' && controllah.gameArray[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[0][0].eXo === 'X' && controllah.fireGame.board[1][0].eXo === 'X' && controllah.fireGame.board[2][0].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[0][1].eXo === 'X' && controllah.fireGame.board[1][1].eXo === 'X' && controllah.fireGame.board[2][1].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[0][2].eXo === 'X' && controllah.fireGame.board[1][2].eXo === 'X' && controllah.fireGame.board[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
 			//Diagonal Check for X
-			else if(controllah.gameArray[0][0].eXo === 'X' && controllah.gameArray[1][1].eXo === 'X' && controllah.gameArray[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
-			else if(controllah.gameArray[0][2].eXo === 'X' && controllah.gameArray[1][1].eXo === 'X' && controllah.gameArray[2][0].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[0][0].eXo === 'X' && controllah.fireGame.board[1][1].eXo === 'X' && controllah.fireGame.board[2][2].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+			else if(controllah.fireGame.board[0][2].eXo === 'X' && controllah.fireGame.board[1][1].eXo === 'X' && controllah.fireGame.board[2][0].eXo === 'X'){return z = 8, controllah.result = "X Won";}
+
+			controllah.fireGame.$save();
 		}
 		function checkWinO(){
 			//  Horizontal Check for O
-			if(controllah.gameArray[0][0].eXo === 'O' && controllah.gameArray[0][1].eXo === 'O' && controllah.gameArray[0][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
-			else if(controllah.gameArray[1][0].eXo === 'O' && controllah.gameArray[1][1].eXo === 'O' && controllah.gameArray[1][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
-			else if(controllah.gameArray[2][0].eXo === 'O' && controllah.gameArray[2][1].eXo === 'O' && controllah.gameArray[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			if(controllah.fireGame.board[0][0].eXo === 'O' && controllah.fireGame.board[0][1].eXo === 'O' && controllah.fireGame.board[0][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[1][0].eXo === 'O' && controllah.fireGame.board[1][1].eXo === 'O' && controllah.fireGame.board[1][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[2][0].eXo === 'O' && controllah.fireGame.board[2][1].eXo === 'O' && controllah.fireGame.board[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
 			// Vertical Check for O
-			else if(controllah.gameArray[0][0].eXo === 'O' && controllah.gameArray[1][0].eXo === 'O' && controllah.gameArray[2][0].eXo === 'O'){return z = 8, controllah.result = "O Won";}
-			else if(controllah.gameArray[0][1].eXo === 'O' && controllah.gameArray[1][1].eXo === 'O' && controllah.gameArray[2][1].eXo === 'O'){return z = 8, controllah.result = "O Won";}
-			else if(controllah.gameArray[0][2].eXo === 'O' && controllah.gameArray[1][2].eXo === 'O' && controllah.gameArray[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[0][0].eXo === 'O' && controllah.fireGame.board[1][0].eXo === 'O' && controllah.fireGame.board[2][0].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[0][1].eXo === 'O' && controllah.fireGame.board[1][1].eXo === 'O' && controllah.fireGame.board[2][1].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[0][2].eXo === 'O' && controllah.fireGame.board[1][2].eXo === 'O' && controllah.fireGame.board[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
 			//Diagonal Check for O
-			else if(controllah.gameArray[0][0].eXo === 'O' && controllah.gameArray[1][1].eXo === 'O' && controllah.gameArray[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
-			else if(controllah.gameArray[0][2].eXo === 'O' && controllah.gameArray[1][1].eXo === 'O' && controllah.gameArray[2][0].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[0][0].eXo === 'O' && controllah.fireGame.board[1][1].eXo === 'O' && controllah.fireGame.board[2][2].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+			else if(controllah.fireGame.board[0][2].eXo === 'O' && controllah.fireGame.board[1][1].eXo === 'O' && controllah.fireGame.board[2][0].eXo === 'O'){return z = 8, controllah.result = "O Won";}
+
+			controllah.fireGame.$save();
 		}
 
 
@@ -93,20 +99,27 @@ ExoController.$inject = ['$firebase'];
 				z++; 
 			}
 			console.log(z);
+
+			controllah.fireGame.$save();
 		}	
 
 		function resetGame(){
-			controllah.gameArray = newGameBoard();
+			controllah.fireGame.board = newGameBoard();
 			z = 0;
 			controllah.result = '';
+
+			controllah.fireGame.$save();
 		}
 
 		function newGameBoard(){
-			return [
-				[ {eXo: ''}, {eXo: ''}, {eXo: ''} ],
+			console.log(controllah.fireGame);
+			controllah.fireGame.board = [
+				[ {eXo: 'X'}, {eXo: ''}, {eXo: ''} ],
 				[ {eXo: ''}, {eXo: ''}, {eXo: ''} ],
 				[ {eXo: ''}, {eXo: ''}, {eXo: ''} ]
 			];
+
+			controllah.fireGame.$save();
 		}
 
 	}
